bf78f876118579ed4b1fb6187c2e6825097085c6
changkanny <105914573+changkanny@users.noreply.github.com>
2025-02-16 03:32:48 +0900

    404、500を修正

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,18 @@
-import Footer from "@/component/footer";
-import Header from "@/component/header";
 import Image from 'next/image';
 
 export default function NotFound() {
+
     return (
         <div>
             <title>Not Found | Canimalocanis</title>
-            <Header />
             <main>
                 <div style={{ position: 'relative', width: '100%', height: '0', paddingBottom: '75%' }}>
                     <Image src="/404.png" alt="404 Not Found" layout="fill" objectFit="contain" />
                 </div>
                 <div className="text-center my-5">
-                    <span>お探しのページは存在しません</span>
+                    <span>お探しのページが見つかりませんでした</span>
                 </div>
             </main>
-            <Footer />
         </div>
     );
 }; 
